226cfea97c55ebe3f27f95ad15662661fd34739f
Saksham Lamichhane <sakshamlamichhane456@gmail.com>
2026-07-27 17:23:00 +0545

    Initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# WealthCraft
+
+A starter project structure for WealthCraft.
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "WealthCraft starter project" << std::endl;
+    return 0;
+}
